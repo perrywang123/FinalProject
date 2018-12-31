f7934bcc48dda509b7d9e750d0626b43a29dfc30
perrywang123 <1731611872@qq.com>
2018-12-31 15:02:31 +0800

    desc

--- Voting_geth/migrations/2_deploy_contracts.js
+++ Voting_geth/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var Voting = artifacts.require("./Voting.sol");
 module.exports = function(deployer) {
-  deployer.deploy(Voting, ['Perry', 'Backy', 'Babe'], {gas: 6700000});
+  deployer.deploy(Voting, ['0x5065727279', '0x4261636b79', '0x42616265'], {gas: 6700000});
 };
 /* As you can see above, the deployer expects the first argument to   be the name of the contract followed by constructor arguments. In our case, there is only one argument which is an array of
 candidates. The third argument is a hash where we specify the gas required to deploy our code. The gas amount varies depending on the size of your contract.
